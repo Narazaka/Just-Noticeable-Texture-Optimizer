574b2054c23b92dc79300a55d37166f3aca91198
Narazaka <info@narazaka.net>
2026-04-21 04:53:33 +0900

    feat(jnto): add TextureOptimizer component

---
+++ Runtime/TextureOptimizer.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using UnityEngine;
+using VRC.SDKBase;
+
+namespace Narazaka.VRChat.Jnto
+{
+    [DisallowMultipleComponent]
+    [AddComponentMenu("Just-Noticeable Texture Optimizer/Texture Optimizer")]
+    public class TextureOptimizer : MonoBehaviour, IEditorOnly
+    {
+        public TextureOptimizerMode Mode = TextureOptimizerMode.Root;
+
+        [Tooltip("品質プリセット (Root必須、Override空可)")]
+        public QualityPresetOverride Preset = new QualityPresetOverride { HasValue = true, Value = QualityPreset.Medium };
+
+        [Tooltip("想定最近接視距離 cm (Root必須、Override空可)")]
+        public FloatOverride ViewDistanceCm = new FloatOverride { HasValue = true, Value = 30f };
+
+        [Tooltip("ボーン重み (Root必須、Override空可)")]
+        public BoneWeightMapOverride BoneWeights = new BoneWeightMapOverride { HasValue = true, Value = BoneWeightMap.Default() };
+
+        public ScriptableObject ComplexityStrategy;
+
+        public List<Object> ExcludeList = new List<Object>();
+    }
+
+    [System.Serializable] public struct QualityPresetOverride { public bool HasValue; public QualityPreset Value; }
+    [System.Serializable] public struct FloatOverride { public bool HasValue; public float Value; }
+    [System.Serializable] public class BoneWeightMapOverride { public bool HasValue; public BoneWeightMap Value; }
+}
